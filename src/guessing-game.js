00518d345217bdaea18296c1b841e9d83425a45f
DenisEsipko <deni007a1@gmail.com>
2021-08-12 16:04:28 +0300

    Task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.answer = null;
+    }
 
+    //Этот метод принимает мин и максимальное значение диапазона номера, чтобы угадать
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
+//Этот метод возвращает кандидата решения
+// (вы делаете предположение на основе диапазона и предыдущих допущений)
     guess() {
-
+        this.answer = Math.round((this.max  + this.min) / 2);
+        return this.answer;
     }
 
+    //Этот метод называется, если REV Call of Heggn () возвращаемый номер, который больше, чем ответ
     lower() {
-
+        this.max = this.answer;
     }
 
+    //Этот метод называется, если предыдущий звонок догадается () возвращаемый номер, который ниже ответа
     greater() {
-
+        this.min = this.answer;
     }
 }
 
